3dc5f83f632abd35945742b1ee12f13fc6100a82
SIhan_Li <5ih4n.san@gmail.com>
2024-05-16 23:08:39 +0200

    aded account settings and privacy, early stage

--- app/account/privacy/page.tsx
+++ app/account/privacy/page.tsx
@@ -1,11 +1,21 @@
-
 import Link from "next/link";
+import { Label } from "@/components/ui/label";
 
 export default async function AccountPage() {
     return (
         <div className="p-3">
-            <header className="text-2xl font-bold text-">konto &gt; sekretess</header>
-            <Link href={""}><p className="bold ">kakor</p><p>hantera hur vi använder dina kakor</p></Link>
+            <header className="text-2xl font-bold">
+            <Link href={"/account"} className="hover:underline underline-offset-4"> 
+                Konto</Link> {">"} integritet
+            </header>
+
+            <div className="mt-8">
+                <Label className="text-xl w-max inline font-bold">Kakor</Label>
+                <header className="w-full mb-8 text-muted-foreground">Hantera hur vi använder kakor. </header>
+
+                <Label className="text-xl w-max inline font-bold">Platsinformation</Label>
+                <header className="w-full mb-8 text-muted-foreground">Hantera den platsinformation som vi använder</header>
+            </div>
         </div>
     )
 }
