b901e5e0877ca886b9583f00e4b51c73cf26b8e7
SIhan_Li <5ih4n.san@gmail.com>
2024-05-16 21:46:47 +0200

    account fixes

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -5,34 +5,34 @@ import Link from "next/link";
 export default async function AccountPage() {
     return (
         <div className="p-3">
-            <header className="text-2xl">Konto</header>
+            <header className="text-2xl font-bold">Konto</header>
             <div className="mt-4">
-                <UserRound className="size-20 bg-slate-700 rounded-md inline-block"/>
-                <div className="inline-block max-w-10 mx-5">
-                    <header>Name</header>
-                    <p className="text-slate-800">display</p>
+                <UserRound className="size-20 bg-secondary rounded-md inline-block"/>
+                <div className="inline-block mx-5">
+                    <header className="w-full text-2xl">Felix Johansson</header>
+                    <p className="text-muted-foreground">@feluxfelixus</p>
                 </div>
-                <Separator className="my-3"/>
+                <Separator className="my-4"/>
                 <div className="my-3">
                     <Link href="" className="block text-xl"><Settings className="inline m-2"/>Konto Inställningar</Link>
                     <Link href="" className="block text-xl"><Cookie className="inline m-2"/>Integritets Inställningar</Link>
                     <Link href="" className="block text-xl"><LockKeyhole className="inline m-2"/>Konto Säkerhet</Link>
                     <Link href="" className="block text-xl"><PartyPopper className="inline m-2"/>Mina Event</Link>
                 </div>
-                <Separator className="my-3"/>
+                <Separator className="my-4"/>
                 <div className="flex">
                     <p className="text-xl w-max inline">Omdömmen</p>
-                    <div className="ml-auto flex w-max"><Star className="fill-black" /><Star className="fill-black"/><Star className="fill-black"/><Star className="fill-black"/><Star/></div>
+                    <div className="ml-auto flex w-max"><Star className="fill-white"/><Star className="fill-white"/><Star className="fill-white"/><Star className="fill-white"/><Star/></div>
                 </div>
-                    <div className="flex">
-                        <UserRound className="size-12 bg-slate-700 rounded-full mt-2"/>
-                        <p className="text-slate-800 m-4">John Doe</p>
-                        <p className="text-slate-800 ml-auto my-auto text-xl">4/5</p>
+                    <div className="flex mt-3">
+                        <UserRound className="size-12 bg-secondary rounded-full mt-2"/>
+                        <p className="text-foreground m-4 text-2xl">Felix Johansson</p>
+                        <p className="text-foreground ml-auto my-auto text-xl">4/5</p>
                     </div>
-                    <div>
-                        <p className="w-max float-left font">Nice</p>
-                        <p className="ml-auto w-max">Balls</p>
-                        <p>Nice</p>
+                    <div className="mt-2">
+                        <p className="w-max float-left font text-xl font-bold">Nice Fest</p>
+                        <p className="ml-auto w-max text-foreground">3 veckor sedan</p>
+                        <p className="mt-3 w-full text-base">Trevlig fest hittade min familj</p>
                     </div>
             </div>
         </div>
