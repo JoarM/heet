a5a1014ad8639e0c982d61308aa07c650f8be2b7
SIhan_Li <5ih4n.san@gmail.com>
2024-05-16 22:53:03 +0200

    hello

---
+++ app/account/privacy/page.tsx
@@ -0,0 +1,11 @@
+
+import Link from "next/link";
+
+export default async function AccountPage() {
+    return (
+        <div className="p-3">
+            <header className="text-2xl font-bold text-">konto &gt; sekretess</header>
+            <Link href={""}><p className="bold ">kakor</p><p>hantera hur vi använder dina kakor</p></Link>
+        </div>
+    )
+}
