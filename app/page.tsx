c9c0dbe8711082eab432d016027a71df4c9e30ce
MicroCreepz <liam.ryden@elev.ga.lbs.se>
2024-05-16 17:15:03 +0200

    add: my position button

--- app/page.tsx
+++ app/page.tsx
@@ -11,28 +11,40 @@ interface Location {
 
 export default function Page() {
   const [location, setLocation] = useState<Location | null>(null)
+  const [zoom, setZoom] = useState(18)
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position)
       setLocation({lat: position?.coords.latitude ?? 0, lng: position?.coords.longitude ?? 0})
     });
   })
 
   return (
     <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}>
-      <div className=' h-[calc(100svh-80px)]'>
+      <div className=' h-[calc(100svh-80px)] relative'>
         <Map
           style={{width: '100%', height: '100%'}}
           defaultCenter={{lat: 0, lng: 0}}
           center={location}
-          defaultZoom={18}
+          zoom={zoom}
+          onZoomChanged={(e) => setZoom(e.detail.zoom)}
           gestureHandling={'greedy'}
           disableDefaultUI={true}
           onCenterChanged={(e) => {
             setLocation({lat: e.detail.center.lat, lng: e.detail.center.lng})
           }}
         />
+        <Button
+        className='absolute rounded-full bottom-1' 
+        variant="secondary"
+        size="icon"
+        onClick={() => {
+          setZoom(18)
+          navigator.geolocation.getCurrentPosition((position) => {
+            setLocation({ lat: position?.coords.latitude ?? 0, lng: position?.coords.longitude ?? 0})
+          });
+        }}
+        ></Button>
       </div>
     
   </APIProvider>
